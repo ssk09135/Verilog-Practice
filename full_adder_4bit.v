2b8eb490c6397d7498715d862dab4417d9772b6a
ssk09315 <ssk6083413@gmail.com>
2025-09-06 17:30:01 +0900

    test in macos

---
+++ full_adder_4bit.v
@@ -0,0 +1,11 @@
+`timescale 1ps/1ps
+module full_adder_4bit (
+    input wire[3:0] A,
+    input wire[3:0] B,
+    input wire Cin,
+    output wire[3:0] S,
+    output wire Cout
+);
+    {Cout, S} = A + B + Cin;
+    
+endmodule
